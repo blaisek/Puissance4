42ddb67a6eda0243cd8d607c090c497351fb3731
Blaise <blaze.kalonda@gmail.com>
2019-09-19 03:07:45 +0200

    UI update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component,  } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'Puissance4';
-  Player1 = 'Player 1: red';
-  Player2 = 'Player 2: yellow';
+export class AppComponent  {
+
+  title = 'Puissance 4';
+
 }
